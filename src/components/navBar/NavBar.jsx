32a9a45c3cbd56411152fa1b423cd6d0d26cff3e
Juan Ignacio Martinez <juanignaciomartinez3@gmail.com>
2022-11-23 00:14:13 -0300

    primer entrega

---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import CartWidget from '../cartWidget/CartWidget';
+
+const NavBar = () => {
+    return <div className='container'>
+        <nav className="navbar navbar-expand-lg ">
+            <div className="container-fluid ">
+                <a className="navbar-brand fs-2 text-primary" href="#">The Wine Cellar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav" >
+                        <li className="nav-item">
+                            <a className="nav-link fs-4"  href="#">Tintos</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link fs-4" href="#">Blancos</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link fs-4" href="#">Espumosos</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className='m-2'>
+                    <CartWidget />
+                </div>
+
+            </div>
+
+        </nav>
+    </div>
+}
+
+
+export default NavBar;
